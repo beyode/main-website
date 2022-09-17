55982b9cc0231e424ef70e8d25300cb52e631a31
Moses Gathuku <mosesgathuku95@gmail.com>
2022-09-17 22:52:07 +0300

    update btn text

--- pages/index.vue
+++ pages/index.vue
@@ -35,7 +35,7 @@
         </div>
         <div class="mt-5 sm:mt-8 sm:flex sm:justify-center">
           <div class="rounded-md">
-            <a href="mailto:moses@beyode.co.ke" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-full text-white bg-red-600 hover:bg-red-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10">
+            <a href="mailto:moses@beyode.co.ke" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base leading-6 font-medium rounded-full text-white bg-red-600 hover:bg-red-300 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out md:py-4 md:text-lg md:px-10 font-bold">
               Email Moses
             </a>
           </div>
